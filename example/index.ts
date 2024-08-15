4da710a448db7695fde775d12eea943c02a56046
Nick Thomas <nichoth@nichoth.com>
2024-08-14 20:00:59 -0700

    fix example

--- example/index.ts
+++ example/index.ts
@@ -1,5 +1,7 @@
 import '../src/index.css'
 import '../src/index.js'
+import '@substrate-system/css-util'
+import '@substrate-system/css-util/visually-hidden'
 
 document.body.innerHTML += `
     <arrow-next></arrow-next>
